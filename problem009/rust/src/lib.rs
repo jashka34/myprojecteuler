6752c98342623dee7fc4ef669e46f2d1e713731f
jashka34 <a_cherniadiev@mail.ru>
2021-08-02 20:20:00 +0000

    Problem 009 solved

--- problem009/rust/src/lib.rs
+++ problem009/rust/src/lib.rs
@@ -1,2 +1,2 @@
-pub mod Pif;
+pub mod pif;
 
--- problem009/rust/src/main.rs
+++ problem009/rust/src/main.rs
@@ -1,4 +1,26 @@
+use rust::pif::Pif;
 fn main() {
     println!("Problem 009");
 
+    // let p: Pif = Pif::new(3, 4, 26);
+    // let ch = p.check();
+    // println!("{:?}", ch);
+
+    for a in 1..499 {
+        for b in a..500 {
+            for c in b..500 {
+                let p: Pif = Pif::new(a, b, c);
+                let ch = p.check();
+                match ch {
+                    Ok(res) => {
+                        // println!("res={}", res);
+                        if a + b + c == 1000 {
+                            println!("{} {} {}, res={} a*b*c={}", a, b, c, res, a * b * c);
+                        }
+                    }
+                    Err(_) => {}
+                }
+            }
+        }
+    }
 }
